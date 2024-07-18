4f21ad9833429aa9d7b6c4380a50d5f3d0fe8f02
NicolausFeldtmann <n.feldtmann@yahoo.dem>
2024-07-18 19:44:57 +0200

    CSS porgres

--- templates.js
+++ templates.js
@@ -1,15 +1,26 @@
 function getEntryTemplates(titel, price, likes, autor, year, genre) {
     return `
         <div class="entry">
-            <h3>Name: ${titel}</h3><hr>
-            <img class="entryImg" src="./assets/img/book2.png"><hr>
+            <div class="entryHeader">
+                <h3>${titel}</h3>
+            </div><hr>
+            <div class="imgArea">
+            <img class="entryImg" src="./assets/img/book2.png">
+            </div><hr>
             <div class="entryMain">
-                Price: ${price}
-                ${likes}<br>
+                <div class="entryNav">Price: ${price}
+                <div class="likes">${likes}<br></div></div>
                 <div class="BookInfo">
-                    Autor: ${autor}<br>
-                    Erscheinugsjahr: ${year}<br>
-                    Genre: ${genre}
+                    <table class="infoTable">
+                        <tr>
+                            <td>Autor</td> <td>:${autor}</td>
+                        </tr>
+                        <tr>
+                            <td>Erscheinugsjahr</td> <td>:${year}</td>
+                        </tr>
+                        <tr>
+                            <td>Genre</td> <td>:${genre}</td>
+                        </tr>
                 </div>
             </div>
         </div>`;
