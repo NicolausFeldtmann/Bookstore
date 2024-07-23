1120a8faa0252c62e513ef757dbc9f8731fe0e4b
NicolausFeldtmann <n.feldtmann@yahoo.dem>
2024-07-23 23:59:09 +0200

    CSS release state

--- templates.js
+++ templates.js
@@ -33,7 +33,9 @@ function getEntryTemplates(titel, price, likes, autor, year, genre) {
             <div Class="inputfield">
                 <input class="nameInput" placeholder="Benutzername" id="nameInput" type="text">
                 <textarea class="commentInput" placeholder="Dein Komentar..." id="commentInput" type="text"></textarea>
-                <img class="commentImg" id="commentImg" src="./assets/img/pin.png">
+                <div class="commentImgSection">
+                    <img class="commentImg" id="commentImg" src="./assets/img/pin.png">
+                </div>
             </div>
         </div>`; 
 }
